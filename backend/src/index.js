8105ea8d258fad45f79b22d21fcc557ba59da518
RudraIIT <b22125@students.iitmandi.ac.in>
2025-01-11 11:42:08 +0530

    undone scaling

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,65 +13,46 @@ import { app } from './socketIO/index.js';
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 
-import cluster from 'cluster';
-import os from 'os';
+dotenv.config();
 
-const totalCPUs = os.cpus().length;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
+const deploy_dirname = path.resolve();
 
-    for (let i = 0; i < totalCPUs; i++) {
-        cluster.fork();
-    }
+app.set('trust proxy', 1);
 
-    cluster.on("exit", worker => {
-        console.log(`Worker ${worker.process.pid} is died`);
-        console.log("Let's fork another worker!");
-        cluster.fork();
-    });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use(express.static(path.join(deploy_dirname, '/frontend/dist')))
+app.use(cookieParser());
+app.use(cors({
+    origin: "https://chat-app-zegp.onrender.com",
+    credentials: true,
+}));
+
+app.use('/api/users',userRoutes);
+app.use('/api/messages',messageRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(deploy_dirname, "frontend","dist","index.html"));
+    })
 } else {
-    dotenv.config();
-
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-
-    const deploy_dirname = path.resolve();
-
-    app.set('trust proxy', 1);
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-    app.use(express.static(path.join(deploy_dirname, '/frontend/dist')))
-    app.use(cookieParser());
-    app.use(cors({
-        origin: "https://chat-app-zegp.onrender.com",
-        credentials: true,
-    }));
-
-    app.use('/api/users', userRoutes);
-    app.use('/api/messages', messageRoutes);
-
-    if (process.env.NODE_ENV === 'production') {
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(deploy_dirname, "frontend", "dist", "index.html"));
-        })
-    } else {
-        app.get('/', (req, res) => {
-            res.send('API is running...');
-        })
-    }
-
-    connectDB()
-        .then(() => {
-            server.listen(process.env.PORT || 3000, () => {
-                console.log(`Server is running on port ${process.env.PORT || 3000}`)
-            })
-        })
-        .catch((error) => {
-            console.log('Error connecting to the database', error.message);
-        })
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    })
 }
+
+connectDB()
+    .then(() => {
+        server.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 3000}`)
+        })
+    })
+    .catch((error) => {
+        console.log('Error connecting to the database', error.message);
+    })
 
 
